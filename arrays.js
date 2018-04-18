b479e2792c07ba3148065d68f374698c00fda278
Assel  Norboeva <assel.qabylqari@gmail.com>
2018-04-18 15:24:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -36,3 +36,9 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
   //return newarr4;
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop();
+  return array;
+}
+
